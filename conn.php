c9418b0776d0e3eb3cec360f09c3e77ea5dd245c
muxiaofefei <1277475536@qq.com>
2017-11-03 10:50:11 +0800

    mysql数据库配置文件
    mysql数据库配置文件

---
+++ conn.php
@@ -0,0 +1,5 @@
+<?php 
+$mysql_server_name='localhost'; //改成自己的mysql数据库服务器
+$mysql_username='root'; //改成自己的mysql数据库用户名
+$mysql_password='root'; //改成自己的mysql数据库密码
+$mysql_database='wangyiyunyinyue'; //改成自己的mysql数据库名
